450c0d25ef9b04f6a4920f2ffb01358c61ad2864
w-wander-r <wanderexe68@gmail.com>
2025-12-16 15:25:11 +0200

    basic try catch added

--- Program.cs
+++ Program.cs
@@ -8,6 +8,43 @@
             using (File.Create("no_file.txt")) { }
             using (File.Create("bad_data.txt")) { }
             using (File.Create("overflow.txt")) { }
+            
+            for (int i = 10; i <= 29; i++)
+            {
+                string fileName = $"{i}.txt";
+
+                try
+                {
+                    string[] lines = File.ReadAllLines(fileName);
+
+                    try
+                    {
+                        // first two lines into int
+                        int num1 = int.Parse(lines[0]);
+                        int num2 = int.Parse(lines[1]);
+
+                        try
+                        {
+                            // todo
+                        }
+                        catch (Exception)
+                        {
+                            // todo
+                        }
+                    }
+                    catch (FormatException)
+                    {
+                        // if can't read that data
+                        File.AppendAllText("bad_data.txt", fileName + Environment.NewLine);
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    // if file not found
+                    File.AppendAllText("no_file.txt", fileName + Environment.NewLine);
+                }
+            }
+
         }
         catch (Exception ex)
         {
