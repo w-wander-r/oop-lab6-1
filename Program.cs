147a7726e72e0889d5fab06379858a3b0e153fb1
w-wander-r <wanderexe68@gmail.com>
2025-12-16 14:50:45 +0200

    try catch for default files('no_file.txt bad_data.txt overflow.txt')

--- Program.cs
+++ Program.cs
@@ -2,5 +2,17 @@
 {
     static void Main()
     {
+        try
+        {
+            // writing/rewriting files
+            using (File.Create("no_file.txt")) { }
+            using (File.Create("bad_data.txt")) { }
+            using (File.Create("overflow.txt")) { }
+        }
+        catch (Exception ex)
+        {
+            // error writing/rewriting files no_file.txt, bad_data.txt, overflow.txt
+            Console.WriteLine($"error: can't write or | rewrite files {ex.Message}");
+        }
     }
 }
